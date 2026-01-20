1b067ad68c077f432f64e0d14c1a07b234824081
nanqipro <2280285631@qq.com>
2026-01-20 22:33:11 +0800

    1

---
+++ backend/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,34 @@
+package com.sky.service.impl;
+
+
+import com.sky.dto.SetmealDTO;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SetmealServiceImpl implements SetmealService {
+    @Autowired
+    private SetmealMapper setmealMapper;
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+    @Autowired
+    private DishMapper dishMapper;
+
+    /**
+     * 新增套餐 同时需要保存套餐和菜品的关联关系
+     * @param setmealDTO
+     */
+    @Override
+    public void saveWithDish(SetmealDTO setmealDTO) {
+
+
+
+
+    }
+}
