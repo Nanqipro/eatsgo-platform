7ed9fe7df153b124da6472eda3925deb68c3efae
Nanqipro <2280285631@qq.com>
2026-01-23 15:37:16 +0800

    1

--- backend/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ backend/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,14 +1,23 @@
 package com.sky.service.impl;
 
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
+import io.netty.handler.codec.MessageAggregationException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.MessageConstraintException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -53,12 +62,44 @@ public class SetmealServiceImpl implements SetmealService {
         // 保存套餐和菜品的关联关系
         setmealDishMapper.insertBatch(setmealDishes);
 
+    }
 
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        int pageNum = setmealPageQueryDTO.getPage();
+        int pageSize = setmealPageQueryDTO.getPageSize();
+        PageHelper.startPage(pageNum,pageSize);
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
 
+    /**
+     * 批量删除套餐
+     * @param ids
+     */
+    @Override
+    @Transactional
+    public void delete(List<Long> ids) {
+        ids.forEach(id -> {
+            Setmeal setmeal = setmealMapper.getById(id);
+            if(setmeal.getStatus() == StatusConstant.ENABLE){
+                // 套餐处于起售状态，不能删除
+                throw new RuntimeException(MessageConstant.SETMEAL_ON_SALE);
+            }
+        });
 
-
-
-
+        // 批量删除套餐
+        ids.forEach(setmealId -> {
+            // 删除套餐表中的数据
+            setmealMapper.deleteById(setmealId);
+            // 删除套餐和菜品的关联数据
+            setmealDishMapper.deleteBySetmealId(setmealId);
+        });
 
 
     }
